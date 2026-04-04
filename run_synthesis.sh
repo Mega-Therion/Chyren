45e0141709bcd6da076126ff3ea3007a3e6e5401
Mega-Therion <mega@omegA.local>
2026-04-04 15:37:42 -0500

    feat: implement identity synthesis pipeline to generate foundational understanding from Neon memory entries

---
+++ run_synthesis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export OMEGA_DB_URL='postgresql://neondb_owner:npg_HbW1Zlkjd7NI@ep-sweet-glade-anvm0pwn.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require'
+python3 chyren_py/identity_synthesis.py
