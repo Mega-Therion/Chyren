2af9496813cc4808d9b74c3dab516b78b8ecaff7
mega <mega_therion@yahoo.com>
2026-04-09 21:21:06 -0500

    chore(structure): flatten project hierarchy for deployment clarity

--- omega_workspace/scripts/launch-chyren.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Fixed Chyren launch script
-WORKSPACE_DIR=$(dirname "$0")/../workspace/OmegA-Next
-echo "OmegA Workspace: $WORKSPACE_DIR"
-cd "$WORKSPACE_DIR" || exit 1
-cargo run -- "$@"
